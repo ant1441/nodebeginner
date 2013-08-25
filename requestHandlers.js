a328ef05789b971f47fcec60a015eaff0f044a9b
Adam <ant1441@gmail.com>
2013-08-25 20:21:36 +0100

    Complete app. Uploading & showing images

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,22 @@
 var querystring = require("querystring"),
-    fs = require("fs");
+    fs = require("fs"),
+    formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
 
-    var body = '<!DOCTYPE html>\n' +
-        '<html>\n' +
-        '\t<head>\n' +
-        '\t\t<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />\n' +
-        '\t</head>\n' +
-        '\t<body>\n' +
-        '\t\t<form action="/upload" method="post">\n' +
-        '\t\t\t<textarea name="text" rows="20" cols="60"></textarea>\n' +
-        '\t\t\t<input type="submit" value="Submit Text" />\n' +
-        '\t\t</form>\n' +
-        '\t</body>\n' +
-        '</html>\n';
+    var body = '<!DOCTYPE html>' +
+        '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" enctype="multipart/form-data" method="post">' +
+        '<input type="file" name="upload" multiple="multiple" />' +
+        '<input type="submit" value="Upload file" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
@@ -23,13 +24,27 @@ function start(response, postData) {
 
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    var content = "You've sent: " + querystring.parse(postData).text;
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(content);
-    response.end();
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function (error, fields, files) {
+        console.log("parsing done");
+
+        /* Possible error on Windows systems: tried to rename an already exxisting file */
+        fs.rename(files.upload.path, "/tmp/test.png", function (error) {
+            if (error) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image: <br />");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
 function show(response) {
