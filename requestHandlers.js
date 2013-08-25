45a3ca5d8e12ca85b2c828c02ce2de4932fd1617
Adam <ant1441@gmail.com>
2013-08-25 19:01:02 +0100

    Serving actual content

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,14 +3,22 @@ var exec = require("child_process").exec;
 function start(response) {
     console.log("Request handler 'start' was called.");
 
-    var content = "empty";
+    var body = '<!DOCTYPE html>\n' +
+        '<html>\n' +
+        '\t<head>\n' +
+        '\t\t<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />\n' +
+        '\t</head>\n' +
+        '\t<body>\n' +
+        '\t\t<form action="/upload" method="post">\n' +
+        '\t\t\t<textarea name="text" rows="20" cols="60"></textarea>\n' +
+        '\t\t\t<input type="submit" value="Submit Text" />\n' +
+        '\t\t</form>\n' +
+        '\t</body>\n' +
+        '</html>\n';
 
-    exec("ls -lah", function (error, stdout, stderr) {
-        content = stdout;
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(content);
-        response.end();
-    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 
 }
 
