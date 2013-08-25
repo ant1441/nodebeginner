15db76f0a4853815c0699bff26b831678ca0b3a0
Adam <ant1441@gmail.com>
2013-08-25 18:45:24 +0100

    Non blocking request handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,26 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
     console.log("Request handler 'start' was called.");
 
-    function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliSeconds);
-    }
-    sleep(10000);
-    return "Hello Start";
+    var content = "empty";
+
+    exec("ls -lah", function (error, stdout, stderr) {
+        content = stdout;
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(content);
+        response.end();
+    });
+
 }
 
-function upload() {
+function upload(response) {
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+    var content = "Hello Upload";
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(content);
+    response.end();
 }
 
 exports.start = start;
